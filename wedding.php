88d6643b3e49ed5eb90ad828df6b7f736de93701
Robert Wieser <robert.wieser@gmail.com>
2013-11-24 14:43:49 +1100

    changed nav to include on wedding.php

---
+++ wedding.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html> 
+<html dir="ltr" lang="en-US"> 
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>Wedding - Robert &amp; Adele</title>
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
+    <link rel="stylesheet" type="text/css" media="all" href="media/css/reset.css" />
+    <link rel="stylesheet" type="text/css" media="all" href="media/css/common.css" />
+    <link rel="stylesheet" type="text/css" media="all" href="media/css/colorbox.css" />
+    <link rel="stylesheet" type="text/css" media="all" href="media/css/font-awesome.css" />
+
+    <!-- Skin -->
+    <link rel="stylesheet" type="text/css" media="all" id="skin_css" href="media/css/charcoal_peach.css" />
+
+   <link rel="stylesheet" type="text/css"
+        media="screen and (max-width: 600px)"
+        href="media/css/mobile.css" />        
+    
+    <link rel="stylesheet" type="text/css"
+        media="screen and (min-width: 601px)"
+        href="media/css/default.css" />
+
+    <link rel="stylesheet" type="text/css"
+    	media="screen and (max-width: 700px)"
+    	href="media/css/small.css" />  
+
+    <!-- Google Web Fonts (Bitter & Sofia) -->
+ 	<link href='http://fonts.googleapis.com/css?family=Bitter:400,400italic,700|Sofia' rel='stylesheet' type='text/css'>
+    
+    <!-- Adaptive Images -->
+    <script>document.cookie='resolution='+Math.max(screen.width,screen.height)+'; path=/';</script>    
+
+    <!-- HTML5 Support in IE8 and lower -->
+    <!--[if lt IE 9]>
+        <script src="media/js/html5shiv.js" type="text/javascript"></script>
+        <script src="media/js/selectivizr-min.js" type="text/javascript"></script>
+        <script src="media/js/PIE.js" type="text/javascript"></script>
+        <link rel="stylesheet" type="text/css" media="all" href="media/css/default.css" />
+        <link rel="stylesheet" type="text/css" media="all" href="media/css/ie8.css" />
+    <![endif]-->
+
+    <!--[if IE 7]>
+    	<link rel="stylesheet" type="text/css" media="all" href="media/css/font-awesome-ie7.css" />
+	<![endif]-->    	
+</head>
+<body class="archive stripes">
+
+<?php
+    include 'navigation.php';
+?>
+	<div id="container" class="wrapper">		
+
+		<!-- Header -->
+		<header id="header" class="page">
+			<div id="site_title">Robert <span class="ampersand">&</span> Adele</div>
+
+			<div id="page_title">
+				<a href="wedding.html"><h1>Wedding</h1></a>
+			</div>	
+		</header>
+		<!-- End header -->
+
+		<!-- Main content area -->
+		<div id="main">
+
+			<!-- Page -->	
+			<div class="post panel">
+				
+				<div class="entry">					
+
+				        <div class="google_map" 
+					        data-longtitude="150.4171167" 
+					        data-latitude="-34.4686667" 
+					        data-zoom="12" 
+					        data-marker="Gibraltar Hotel">
+				        </div>
+
+					<hr />
+
+					<div class="column_66">
+						<h3>Ceremony</h3>
+						<p>Ovid quatur rese corro blanis nes susaperor re maio moditib usciet ipsum dolo culparit maio.</p>
+						<p><strong>Saturday 19th April 2014</strong><br />
+							Three o'clock in the afternoon<br />
+							Gibraltar Hotel<br />
+							Bowral, New South Wales
+						</p>													
+						<p>Ovid quatur rese corro blanis nes susaperor re maio moditib usciet ipsum dolo culparit maio. Ovid quatur rese corro blanis nes susaperor re maio moditib usciet ipsum dolo culparit maio.</p>
+					</div>
+
+					<div class="column_33 column_last">
+						<h3>Gift Registry</h3>
+						<p>Ovid quatur rese corro blanis nes susaperor re maio moditib usciet ipsum dolo culparit maio.</p>
+						<ul class="centered registry_list">
+							<li><a href="">myer.com.au</a></li>
+							<li><a href="">anaconda.com.au</a></li>
+						</ul>	
+					</div>
+
+				</div>
+
+				<footer class="post_meta">
+
+					<div class="post_date">Posted by <a href="#"><i class="icon-user"></i> Wes</a> on September 13, 2012</div>
+					<div class="tags"><i class="icon-tag"></i> <a href="#">Wedding</a>, <a href="#">News</a></div>
+
+				</footer>
+
+				<div class="waves"></div>					
+
+			</div>
+			<!-- End page entry -->
+
+		</div>
+		<!-- End main content area -->
+
+		<!-- Footer -->
+		<footer id="pre_footer">
+
+			<div class="connect">
+				<h4>Connect</h4>
+				<div class="email"><a href="mailto:hello@yourwedding.com">hello@yourwedding.com</a></div>
+				
+				<ul class="social horizontal">
+					<li><a href="" class="facebook social_button">Facebook</a></li>
+					<li><a href="" class="twitter social_button">Twitter</a></li>
+					<li><a href="" class="flickr social_button">Flickr</a></li>
+					<li><a href="" class="rss social_button">RSS</a></li>
+					<li><a href="" class="youtube social_button">YouTube</a></li>
+				</ul>
+			</div>
+
+		</footer>
+		<!-- End footer -->
+
+	</div>
+
+	<footer id="footer">
+
+		<div class="wrapper">
+
+			<div class="double_dotted">
+				<h4>R <span class="ampersand">&</span> A</h4>
+			</div>
+
+			<div class="bottom">
+				<span class="float_left">&copy; Copyright 2012</span>
+				<a class="float_right">Hitched theme by <span class="osfa">Studio 164a</span></a>
+			</div>
+
+		</div>
+
+	</footer>
+
+<!-- Load Javascript right before the closing body tag -->
+
+<!-- jQuery -->
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+
+<!-- Flexnav -->
+<script type="text/javascript" src="media/js/jquery.flexnav.js"></script>
+
+<!-- Dropdown Menus -->
+<script type="text/javascript" src="media/js/jquery.dropdownmenu.js"></script>
+
+<!-- Hover Intent -->
+<script type="text/javascript" src="media/js/jquery.hoverIntent.min.js"></script>
+
+<!-- Image Hover -->
+<script type="text/javascript" src="media/js/jquery.imageHover.js"></script>
+
+<!-- Colorbox -->
+<script type="text/javascript" src="media/js/jquery.colorbox.min.js"></script>
+
+<!-- Google Maps -->
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
+<script type="text/javascript" src="media/js/jquery.ui.map.min.js"></script>
+
+<!-- Hitched default javascript setup -->
+<script type="text/javascript" src="media/js/hitched.js"></script>
+
+<!-- Call scripts -->
+<script>
+(function($) {
+	$(document).ready( function() {		
+		HITCHED.documentReady();
+	});
+
+	$(window).load( function() {	
+		HITCHED.windowLoad();
+	});	
+})(jQuery);
+</script>
+
+</body>
